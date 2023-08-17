48c73f59b6fc5545898b2518f20f24a2f1174322
Soul Of Mo <morinsultan@gmail.com>
2023-08-17 23:41:45 +0100

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+var tgg = document.querySelector(".toggle") ;
+var toggleButton = document.querySelector(".toggle-button");
+
+var body = document.querySelector("body")
+tgg.addEventListener('click', function(){
+    toggleButton.classList.toggle("toggle-active")
+    body.classList.toggle("dark-theme")
+
+})
